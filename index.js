9d110a8c3f16123dcca492cd6f2edd043e62481f
Json <dreammer.4115@gmail.com>
2023-10-18 21:26:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,21 +7,18 @@ exports.handler = async (event) => {
     // Get the object from the event and show its content type
     const bucket = event.Records[0].s3.bucket.name;
     const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
-    console.log("ggggaaaaa", key);
 
     // Get image from S3
     const originalImage = await s3.getObject({ Bucket: bucket, Key: key }).promise();
     console.log(originalImage);
-    // Resize the image using Sharp
+
     const resizedImage = await sharp(originalImage.Body, { animated: true })
       .webp({ effort: 2, quality: 80 })
-      .resize(200, 200) // or any other size
+      .resize(200, 200)
       .toBuffer();
 
-    // Define new filename and metadata if needed
     const targetKey = key.replace(/\.[^/.]+$/, "") + "-thumbnail.webp"; // change extension
 
-    // Upload the resized image back to S3
     await s3
       .putObject({
         Bucket: bucket,
