0110299d30767c95fed37683aa8c03f7cf1f1795
Json <dreammer.4115@gmail.com>
2023-10-20 07:48:38 +0700

    update compress function

--- index.ts
+++ index.ts
@@ -3,34 +3,61 @@ import sharp from "sharp";
 import aws from "aws-sdk";
 const s3 = new aws.S3();
 
+type Metadata = {
+  resize: number;
+  resizeName?: string;
+  resizeWithOriginal?: boolean;
+};
+
 export const handler: Handler<S3Event> = async (event, context) => {
   try {
     // Get the object from the event and show its content type
     const bucket = event.Records[0].s3.bucket.name;
     const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+    const contentType = "image/webp";
 
     // Get image from S3
     const originalImage = await s3.getObject({ Bucket: bucket, Key: key }).promise();
     if (!originalImage?.Body) return;
-    const metadata = originalImage.Metadata?.json && JSON.parse(originalImage.Metadata?.json);
-    for (const k in metadata) {
-      if (typeof metadata[k] === "string") metadata[k] = decodeURIComponent(metadata[k]);
-    }
-    const resizedImage = await sharp(originalImage.Body as Buffer, { animated: true })
-      .webp({ effort: 2, quality: 80 })
-      .resize(200, 200)
-      .toBuffer();
+    const metadata: Metadata | undefined =
+      originalImage.Metadata?.json && JSON.parse(originalImage.Metadata?.json);
 
-    const targetKey = key.replace(/\.[^/.]+$/, "") + "-thumbnail.webp"; // change extension
+    for (let k in metadata) {
+      const key = k as keyof Metadata;
+      if (typeof metadata[key] === "string")
+        (metadata[key] as string) = decodeURIComponent(metadata[key] as string);
+    }
+
+    let resizedImage = sharp(originalImage.Body as Buffer, { animated: true }).webp({
+      effort: 2,
+      quality: 80,
+    });
+
+    if (metadata) {
+      const { resize, resizeName, resizeWithOriginal } = metadata;
+      if (resize) {
+        await s3
+          .putObject({
+            Bucket: bucket,
+            Key: resizeName ? key.slice(0, key.lastIndexOf("/") + 1) + resizeName : key,
+            Body: await resizedImage.resize(resize).toBuffer(),
+            ContentType: contentType, // specify the new content type
+          })
+          .promise();
+
+        if (!resizeWithOriginal) return;
+      }
+    }
 
     await s3
       .putObject({
         Bucket: bucket,
-        Key: targetKey,
-        Body: resizedImage,
-        ContentType: "image/webp", // specify the new content type
+        Key: key,
+        Body: await resizedImage.toBuffer(),
+        ContentType: contentType, // specify the new content type
       })
       .promise();
+      
   } catch (error) {
     console.log(error);
     return;
